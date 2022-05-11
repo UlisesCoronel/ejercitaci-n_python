150e318cb8149d809859eb3a2b07c35d4969e805
Ulises Coronel <ulisescoronel@gmail.com>
2022-05-11 11:18:57 -0300

    Add DB + Python

--- Notas.py
+++ Notas.py
@@ -74,3 +74,125 @@ os.rmdir('miCarpeta') #elimina una carpeta
 
 
 
+
+
+
+
+
+
+
+
+
+#----------- BASES DE DATOS
+
+import mysql.connector
+
+midb = mysql.connector.connect( #el método devuelve una instancia de DB
+  host='localhost', #dice que nos vamos a conectar a un servidor local
+  user='UlisesCoronel', #el usuario que se va a conectar
+  password='Ulis3s_Cor0n3l',
+  database='prueba' #nombre de la base de datos
+)
+
+cursor = midb.cursor()
+# el cursor es un objeto que nos permite interactuar con la DB
+# mediante lenguaje SQL
+
+cursor.execute('select * from Usuario')
+#recupera todos los registros guardados en usuario
+
+resultado = cursor.fetchall()
+#guarda todos los resultados la consulta de cursor.execute()
+
+resultado = cursor.fetchone()
+#guarda sólo el PRIMER ELEMENTO
+
+
+print(resultado)
+
+
+
+
+
+#------------------- Consultas a la DB
+
+cursor.execute('show create table usuario')
+#muestra la definición de la tabla
+
+resultado = cursor.fetchall()
+
+print(resultado)
+
+
+
+
+#------------------- insertar nuevos registros
+
+sql = 'insert into usuario (email,username,edad) values (%s,%s,%s)'
+#esta es la instrucción a enviar a sql, los valores se encuentran vacíos
+
+nuevo_usuario = ('nuevo_usuario@correo.com','Nuevo Usuario', 45)
+#parametros que definen el nuevo registro de la DB
+
+cursor.execute(sql,nuevo_usuario)
+#ejecuta a la acción indicada en 'sql'
+
+midb.commit()
+#ejecuta la consulta hecha en cursor.execute
+
+cursor.execute('select * from usuario')
+
+resultado = cursor.fetchall()
+
+print(resultado)
+
+
+
+
+#---------------- Actualizar datos de la db
+
+sql = 'update usuario set email = %s where id = %s'
+#comando para actualizar datos
+value = ('micorreo@otrocorreo.com',4)
+cursor.execute(sql,value)
+
+midb.commit()
+
+cursor.execute('select * from usuario')
+
+consulta = cursor.fetchall()
+
+print(consulta)
+
+
+
+
+
+#-------------- Eliminar datos de la DB
+
+sql = 'delete from usuario where id = %s'
+value = (4,)
+#los valores se pasan siempre como tuplas, así sea solo uno
+
+cursor.execute(sql,value)
+
+midb.commit()
+
+cursor.execute('select * from usuario')
+
+consulta = cursor.fetchall()
+
+print(consulta)
+
+
+
+
+#--------------- Consultas parciales
+
+cursor.execute('select * from usuario limit 2')
+#limit %s indica la cantidad de consultas a realizar
+
+
+resultado = cursor.fetchall()
+
+print(resultado)
